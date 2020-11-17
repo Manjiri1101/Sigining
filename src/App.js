7f70fc4e7fe57ef0d4457296311fcaf34e8e7bba
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2020-11-17 06:47:26 -0800

    reverted chaneges in authentication

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Amplify, { Auth, API } from "aws-amplify";
-import awsconfig from "./aws-exports";
-import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <AmplifySignOut />
-      <h2>My App</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
